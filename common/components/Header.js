46b2b20e67053ecfc0845caa16be45ec518f7d58
Jayadeep KM <kmjayadeep@gmail.com>
2020-04-17 15:39:17 +0530

    new button

--- common/components/Header.js
+++ common/components/Header.js
@@ -4,27 +4,16 @@ export default function () {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <a className="navbar-brand" href="/">xTodo</a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="/">
-            Home
-            <span className="sr-only">(current)</span>
-          </a>
-          {
-            // <a className="nav-item nav-link" href="/">Features</a>
-          }
-        </div>
+      <div className="navbar-nav mr-auto">
+        {/* <a className="nav-item nav-link active" href="/">
+          Home
+        </a> */}
+      </div>
+      <div className="navbar-nav">
+        <a className="nav-item nav-link bg-primary add-button" href="/#new">
+          <i className="fa fa-plus" />
+          <b>NEW</b>
+        </a>
       </div>
     </nav>
   );
--- server/template.js
+++ server/template.js
@@ -7,6 +7,7 @@ export default () => (
         <link rel="icon" href="/assets/favicon.ico">
         <link rel="stylesheet" href="/main.css"/>
         <link rel="stylesheet" href="/assets/bootstrap.min.css">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
       </head>
 
       <body>
