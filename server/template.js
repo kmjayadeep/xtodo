13b1bc043e6b04b0f84ebb249a7a91b2fc88a1c5
Jayadeep KM <kmjayadeep@gmail.com>
2020-04-18 10:37:59 +0530

    add meta tags

--- server/template.js
+++ server/template.js
@@ -4,11 +4,15 @@ export default () => (
     <html>
       <head>
         <title>xTodo</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="xTodo: Manage your tasks and notes">
+        <meta name="keywords" content="todo,tasks,notes,xtodo">
+        <meta name="author" content="Jayadeep KM">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="/assets/favicon.ico">
         <link rel="stylesheet" href="/main.css"/>
         <link rel="stylesheet" href="/assets/bootstrap.min.css">
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
       </head>
 
       <body>
