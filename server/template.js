3c29914511b9e75a4149c409f37f0eb72821e362
Jayadeep KM <kmjayadeep@gmail.com>
2020-04-18 10:04:35 +0530

    site responsive

--- server/template.js
+++ server/template.js
@@ -8,6 +8,7 @@ export default () => (
         <link rel="stylesheet" href="/main.css"/>
         <link rel="stylesheet" href="/assets/bootstrap.min.css">
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
       </head>
 
       <body>
